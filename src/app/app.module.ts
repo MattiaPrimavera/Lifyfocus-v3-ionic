b4f76259caf82c2477715ad991a4aa1b5502ae97
Mattia Primavera <mattia@norbertapp.com>
2019-04-16 01:05:12 +0200

    Adding firebase credentials + declaring deps in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,46 +1,38 @@
+import { TaskListService } from './../providers/task-list-service/task-list-service';
+import { FIREBASE_CONFIG } from './firebase.credentials';
 import { DynamicUnknownComponent } from './../components/dynamic-unknown/dynamic-unknown';
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
-
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { MockProvider } from '../providers/mock/mock';
 import { DynamicTaskListComponent } from '../components/dynamic-task-list/dynamic-task-list';
 import { DynamicObjectiveListComponent } from '../components/dynamic-objective-list/dynamic-objective-list';
 import { DynamicContentComponent } from '../components/dynamic-content/dynamic-content';
-import { ObjectivesPage } from '../pages/objectives/objectives';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
     TabsPage,
     DynamicTaskListComponent,
     DynamicObjectiveListComponent,
     DynamicContentComponent,
     DynamicUnknownComponent,
-    ObjectivesPage
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    ObjectivesPage,
     TabsPage,
     DynamicTaskListComponent,
     DynamicObjectiveListComponent,
@@ -51,7 +43,8 @@ import { ObjectivesPage } from '../pages/objectives/objectives';
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
-    MockProvider
-  ]
+    MockProvider,
+    TaskListService
+  ],
 })
 export class AppModule {}
---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAeDWzHeDPqIVPEKiDOL-vhKjvRqPLFTUY",
+  authDomain: "lifyfocus.firebaseapp.com",
+  databaseURL: "https://lifyfocus.firebaseio.com",
+  projectId: "lifyfocus",
+  storageBucket: "lifyfocus.appspot.com",
+  messagingSenderId: "803614802409"
+}
