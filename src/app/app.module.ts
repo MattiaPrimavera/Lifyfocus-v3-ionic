8aba1d8174297e2d27aad8c085058cb4155ce65a
Mattia Primavera <mattia@norbertapp.com>
2019-04-16 02:01:44 +0200

    Removing dynamic components tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { ToastService } from './../providers/toast.service';
 import { TaskListService } from './../providers/task-list-service/task-list-service';
 import { FIREBASE_CONFIG } from './firebase.credentials';
-import { DynamicUnknownComponent } from './../components/dynamic-unknown/dynamic-unknown';
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
@@ -10,9 +9,6 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { MockProvider } from '../providers/mock/mock';
-import { DynamicTaskListComponent } from '../components/dynamic-task-list/dynamic-task-list';
-import { DynamicObjectiveListComponent } from '../components/dynamic-objective-list/dynamic-objective-list';
-import { DynamicContentComponent } from '../components/dynamic-content/dynamic-content';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 
@@ -20,10 +16,6 @@ import { AngularFireDatabaseModule } from 'angularfire2/database';
   declarations: [
     MyApp,
     TabsPage,
-    DynamicTaskListComponent,
-    DynamicObjectiveListComponent,
-    DynamicContentComponent,
-    DynamicUnknownComponent,
   ],
   imports: [
     BrowserModule,
@@ -35,10 +27,6 @@ import { AngularFireDatabaseModule } from 'angularfire2/database';
   entryComponents: [
     MyApp,
     TabsPage,
-    DynamicTaskListComponent,
-    DynamicObjectiveListComponent,
-    DynamicContentComponent,
-    DynamicUnknownComponent
   ],
   providers: [
     StatusBar,
--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,17 +1,7 @@
 import { NgModule } from '@angular/core';
-import { DynamicContentComponent } from './dynamic-content/dynamic-content';
-import { DynamicTaskListComponent } from './dynamic-task-list/dynamic-task-list';
-import { DynamicObjectiveListComponent } from './dynamic-objective-list/dynamic-objective-list';
-import { DynamicUnknownComponent } from './dynamic-unknown/dynamic-unknown';
 @NgModule({
-	declarations: [DynamicContentComponent,
-    DynamicTaskListComponent,
-    DynamicObjectiveListComponent,
-    DynamicUnknownComponent],
+	declarations: [],
 	imports: [],
-	exports: [DynamicContentComponent,
-    DynamicTaskListComponent,
-    DynamicObjectiveListComponent,
-    DynamicUnknownComponent]
+	exports: []
 })
 export class ComponentsModule {}
