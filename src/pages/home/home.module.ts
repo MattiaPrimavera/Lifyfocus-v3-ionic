ab6815c9ed1d61d5315403c8f87b3a4f69395de4
Mattia <sconqua@gmail.com>
2019-07-02 02:27:46 +0200

    ✨ Show today & high-prio in Board page

--- src/pages/home/home.module.ts
+++ src/pages/home/home.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { MpSlidingItemModule } from '../../components/mp-sliding-item/mp-sliding-item.module';
 import { MpLoaderModule } from '../../components/mp-loader/mp-loader.module';
+import { MpCardModule } from '../../components/mp-card/mp-card.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { MpLoaderModule } from '../../components/mp-loader/mp-loader.module';
   imports: [
     IonicPageModule.forChild(HomePage),
     MpSlidingItemModule,
-    MpLoaderModule
+    MpLoaderModule,
+    MpCardModule
   ],
   exports: [
     HomePage
